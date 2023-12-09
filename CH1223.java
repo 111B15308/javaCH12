e22a35b26b5d7959acacc754fb24120ba88531cf
111B15308 <145731748+111B15308@users.noreply.github.com>
2023-12-09 22:28:28 +0800

    Create CH1223.java

---
+++ CH1223.java
@@ -0,0 +1,8 @@
+public class CH1223{
+    public static void main(String[] args) {
+        double doubleValue = 67.834;
+        String stringValue = Double.toString(doubleValue);
+        int stringLength = stringValue.length();
+        System.out.println(stringLength);
+    }
+}
