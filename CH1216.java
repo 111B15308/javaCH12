d8ab3b176d34824c8d1920ca065f1961a52ca50c
111B15308 <145731748+111B15308@users.noreply.github.com>
2023-12-09 22:17:00 +0800

    Create CH1216.java

---
+++ CH1216.java
@@ -0,0 +1,8 @@
+public class CH1216 {
+    public static void main(String[] args) {
+        String inputString = "262904713";
+        int intValue = Integer.parseInt(inputString);
+        int result = intValue * 2;
+        System.out.println(result);
+    }
+}
