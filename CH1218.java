b752e5c9577848d5b64cf1198bd134908eda66c5
111B15308 <145731748+111B15308@users.noreply.github.com>
2023-12-09 22:19:20 +0800

    Create CH1218.java

---
+++ CH1218.java
@@ -0,0 +1,6 @@
+public class CH1218{
+    public static void main(String[] args) {
+        String inputString = "45874356676541331";
+        long longValue = Long.parseLong(inputString);
+    }
+}
